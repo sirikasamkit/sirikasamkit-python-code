496c51beb3a8beefdde45aa54a0d9083d3668449
sirikasamkit thiemthong <sirikasamkit.t@ku.th>
2025-07-08 12:01:20 +0700

    edit work sirikasamkit

---
+++ assignments/week01/lab01.py
@@ -0,0 +1,13 @@
+print("=== Personal Profile Creator ===")
+Full_name = input("What your name : ")
+age  = input("How old are you : ")
+email = input("What is your email : ")
+phone = input("What is your phone number : ")
+hobby = input("What is your favorite hobby : ")
+
+print("== Your profile ==")
+print("Name : ",Full_name)
+print("Age : ",age)
+print("Email : ",email)
+print("Phone number : ",phone)
+print("Favorite Hobby : ",hobby)
